9cc27d8493b56ac1f1846e4b2f4ba96f518cbd3a
David Mojicevic <david.mojicevic@eng.it>
2018-12-06 11:57:22 +0100

    Adding POJOs

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author David Mojicevic on 12/6/18.
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author David Mojicevic on 12/6/18.
+ */
+public class Vet extends Person{
+
+}
