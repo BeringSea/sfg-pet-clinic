d2dfe8a4dbca6814af3d535a024a24ecd4f4bbee
David Mojicevic <david.mojicevic@eng.it>
2018-12-12 11:45:44 +0100

    Added PetTypeService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author David Mojicevic on 12/12/18.
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
