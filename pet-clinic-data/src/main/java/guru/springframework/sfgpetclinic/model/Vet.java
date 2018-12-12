8d5467ff3006a97eaa5ee71295da586c69bf6f86
David Mojicevic <david.mojicevic@eng.it>
2018-12-12 11:13:08 +0100

    Added specialities, added to vet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,8 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author David Mojicevic on 12/6/18.
  */
 public class Vet extends Person{
 
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
