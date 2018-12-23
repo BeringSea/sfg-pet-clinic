b01a990a3f6661af57ca9fdcd7d078a4f1415316
David Mojicevic <david.mojicevic@eng.it>
2018-12-23 19:48:05 +0100

    Added spring date service for owners

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * @author David Mojicevic on 12/23/18.
  */
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
