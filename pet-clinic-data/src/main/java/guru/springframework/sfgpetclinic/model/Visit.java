ec2fbc8624caba971d7b73a93e7eebaec53cb1f3
David Mojicevic <david.mojicevic@eng.it>
2018-12-23 19:16:13 +0100

    Added Visit Entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * @author David Mojicevic on 12/12/18.
  */
-public class visit extends BaseEntity{
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne()
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
