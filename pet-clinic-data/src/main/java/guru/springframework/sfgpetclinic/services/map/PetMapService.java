16d6a609d67ef57ac05456e8d0acd650316c6b7e
David Mojicevic <david.mojicevic@eng.it>
2018-12-23 20:07:58 +0100

    Added Specialty SD JPA Service

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
@@ -10,7 +10,7 @@ import java.util.Set;
  * @author David Mojicevic on 12/7/18.
  */
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -12,11 +12,11 @@ import java.util.Set;
  * @author David Mojicevic on 12/7/18.
  */
 @Service
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialtyService specialtyService;
 
-    public VetServiceMap(SpecialtyService specialtyService) {
+    public VetMapService(SpecialtyService specialtyService) {
         this.specialtyService = specialtyService;
     }
 
