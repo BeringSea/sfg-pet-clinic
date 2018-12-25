3921d5bf5bf9670fe0dc6a14426ae867c1cce381
David Mojicevic <david.mojicevic@eng.it>
2018-12-25 11:03:38 +0100

    Added Project Lombok @AllArgsConstructor annotation to Person

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import javax.persistence.MappedSuperclass;
  */
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
